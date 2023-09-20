29ed33881801ab6b1def0530786995c1796bfe74
Cédric PRONZOLA <cedric@livingdata.co>
2023-09-20 15:56:12 +0200

    Add secure option for FTP connexions

--- scripts/list-debug.js
+++ scripts/list-debug.js
@@ -6,7 +6,7 @@ import process from 'node:process'
 
 import {listFiles} from '../lib/storage/ftp.js'
 
-const {FTP_HOST, FTP_PORT, FTP_USER, FTP_PASSWORD, FTP_START_PATH} = process.env
+const {FTP_HOST, FTP_PORT, FTP_USER, FTP_PASSWORD, FTP_START_PATH, FTP_SECURE} = process.env
 
 const ftpOptions = {
   host: FTP_HOST,
@@ -14,7 +14,8 @@ const ftpOptions = {
   user: FTP_USER,
   password: FTP_PASSWORD,
   startPath: FTP_START_PATH,
-  verbose: false
+  verbose: false,
+  secure: FTP_SECURE
 }
 
 const tree = await listFiles(ftpOptions)
