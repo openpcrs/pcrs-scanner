1e07c10dc64626fcb4e586ca71a67b9ddfbbac24
Jérôme Desboeufs <jerome@livingdata.co>
2023-10-09 17:22:43 +0200

    Add route to get a specific storage

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ import process from 'node:process'
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import createError from 'http-errors'
 
+import mongo from './lib/util/mongo.js'
 import w from './lib/util/w.js'
 import errorHandler from './lib/util/error-handler.js'
-import {createStorage} from './lib/models/storage.js'
+import {createStorage, getStorage} from './lib/models/storage.js'
+
+await mongo.connect()
 
 const PORT = process.env.PORT || 5000
 
@@ -24,6 +28,28 @@ app.use(cors({origin: true}))
 app.post('/storages', w(async (req, res) => {
   const storage = await createStorage(req.body)
   res.send(storage)
+}))
+
+app.param('storageId', w(async (req, res, next) => {
+  let storageId
+
+  try {
+    storageId = mongo.asObjectId(req.params.storageId)
+  } catch {
+    return next(createError(404, 'Invalid storage identifier'))
+  }
+
+  req.storage = await getStorage(storageId)
+
+  if (!req.storage) {
+    return next(createError(404, 'Storage not found'))
+  }
+
+  next()
+}))
+
+app.get('/storages/:storageId', w(async (req, res) => {
+  res.send(req.storage)
 }))
 
 app.use(errorHandler)
