eb59ca908588049f2cd5e4a16c1d7eb25662a9b7
Oscar Hemelaar <oscar.hemelaar@qonfucius.team>
2025-01-28 17:18:13 +0100

    WIP fix host

--- helloworld.cjs
+++ helloworld.cjs
@@ -1,6 +1,6 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = process.env.PORT;
 
 const server = createServer((req, res) => {
