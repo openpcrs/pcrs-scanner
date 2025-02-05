b826c1692c7e51b53642761782b1e2e6f45042b8
Oscar Hemelaar <oscar.hemelaar@qonfucius.team>
2025-02-05 10:28:18 +0100

    WIP

--- gdal_install.sh
+++ gdal_install.sh
@@ -2,11 +2,14 @@
 
 INSTALL_LOCKFILE="gdal_install.lock"
 
+gdal-config --version
 # check a file to not run into a recursion
 if [[ ! -e "$INSTALL_LOCKFILE" ]]; then
   touch "$INSTALL_LOCKFILE"
   # install gdal with specific options to use the host gdal library
+  # need to remove the previously installed version for it to work correctly, but yarn remove would change the lock file
   rm -rf node_modules/gdal-async
+  # install gdal-async with special flags
   npm_config_build_from_source=true npm_config_shared_gdal=true yarn add gdal-async
   rm "$INSTALL_LOCKFILE"
 fi
