63968be33c59af11dee47928110b9dae8ceb3001
Oscar Hemelaar <oscar.hemelaar@qonfucius.team>
2025-02-07 15:47:45 +0100

    WIP

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: node helloworld.cjs
+web: LD_LIBRARY_PATH="${LD_LIBRARY_PATH}/app/.apt/usr/lib/x86_64-linux-gnu/blas/:/app/.apt/usr/lib/x86_64-linux-gnu/lapack/" node helloworld.cjs
 worker: node worker.js
--- helloworld.cjs
+++ helloworld.cjs
@@ -1,4 +1,5 @@
 const { createServer } = require('node:http');
+const gdal = require('gdal-async')
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT;
@@ -6,7 +7,7 @@ const port = process.env.PORT;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(`${gdal.bundled}`);
 });
 
 server.listen(port, hostname, () => {
